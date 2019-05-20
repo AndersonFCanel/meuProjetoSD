182ab093c12636c92ba15cc44c575cc9e77bca46
Anderson <andersonfcanel@gmail.com>
2019-05-19 23:54:22 -0300

    switch thread

--- meuProjetoSD/src/com/suamSD/Cliente.java
+++ meuProjetoSD/src/com/suamSD/Cliente.java
@@ -19,17 +19,6 @@ public class Cliente extends Thread
 
 	public static void main(String[] args)
     {
-	
-		/*String IP ;
-		
-		switch ( IP) {
-		case value:
-			
-			break;
-
-		default:
-			break;
-		}*/
 		
 		try {
             // Obtendo referência do serviço de registro
@@ -40,17 +29,41 @@ public class Cliente extends Thread
             
             stub.setContaThreads();
             System.out.println("CONTADOR DE THREADS: " + stub.getContaThreads( ));
+            int thread = stub.getContaThreads( );
             
-            stub.setAlfabeto 			 ( );
-            //stub.setEstados  			 ( );
-            stub.setRegra				 ( );
-            //stub.setEstInicial			 ( );
-            //stub.setConjuntoEstadosFinais( );
-            stub.checaPalavra			 ( );
             
-            Thread  t = new Thread(Cliente1); //Cria a linha de execução
-       	    t.start();                 //Ativa a thread
-       	    t.setName( "Cliente: ==> "+ stub.getContaThreads( ));
+            switch (thread) {
+			case 1: 
+				    stub.setAlfabeto 			 ( );
+		            //stub.setEstados  			 ( );
+		            stub.setRegra				 ( );
+		            //stub.setEstInicial			 ( );
+		            //stub.setConjuntoEstadosFinais( );
+		            stub.checaPalavra			 ( );
+		            
+		            Thread  t = new Thread(Cliente1); //Cria a linha de execução
+		       	    t.start();                 //Ativa a thread
+		       	    t.setName( "Cliente: ==> "+ stub.getContaThreads( ));
+				break;
+			case 2:
+				//stub.setAlfabeto 			 ( );
+		        stub.setEstados  			 ( );
+		        //stub.setRegra				 ( );
+		        stub.setEstInicial			 ( );
+		        stub.setConjuntoEstadosFinais( );
+		        //stub.checaPalavra			 ( );
+		        
+		        Thread  t2= new Thread(Cliente2); //Cria a linha de execução
+		   	   t2.start();                 //Ativa a thread
+		   	    t2.setName( "Cliente: ==> "+ stub.getContaThreads( ));
+		        
+				
+
+			default:
+				break;
+			}
+            
+           
             
        	    System.out.println("Fim da execução do cliente!");
 
@@ -66,39 +79,7 @@ public class Cliente extends Thread
         System.out.println("Estado: "     + Thread.currentThread( ).getState( )    );
     
 	
-	try {
-        // Obtendo referência do serviço de registro
-        Registry registro = LocateRegistry.getRegistry(Util.IPSERVIDOR, Util.PORTA);
-
-        // Procurando pelo objeto distribuído registrado previamente com o NOMEOBJDIST
-        AutomatoInterface stub = (AutomatoInterface) registro.lookup(Util.NOMEOBJDIST);
-        
-        stub.setContaThreads();
-        System.out.println("CONTADOR DE THREADS: " + stub.getContaThreads( ));
-        
-        //stub.setAlfabeto 			 ( );
-        stub.setEstados  			 ( );
-        //stub.setRegra				 ( );
-        stub.setEstInicial			 ( );
-        stub.setConjuntoEstadosFinais( );
-        //stub.checaPalavra			 ( );
-        
-        Thread  t = new Thread(Cliente2); //Cria a linha de execução
-   	    t.start();                 //Ativa a thread
-   	    t.setName( "Cliente: ==> "+ stub.getContaThreads( ));
-        
-   	    System.out.println("Fim da execução do cliente!");
-
-    } catch (RemoteException | NotBoundException ex) {
-        Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
-    }
 	
-    
-    //código para executar em paralelo
-    System.out.println("ID: "         + Thread.currentThread( ).getId( )       );
-    System.out.println("Nome: "       + Thread.currentThread( ).getName( )     );
-    System.out.println("Prioridade: " + Thread.currentThread( ).getPriority( ) );
-    System.out.println("Estado: "     + Thread.currentThread( ).getState( )    );
 }
 	
 }
