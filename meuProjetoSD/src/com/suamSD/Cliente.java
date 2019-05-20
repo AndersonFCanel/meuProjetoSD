0078e242b41583c9fbb4daa5e52c7d2636aca475
AndersonCanel <anderson.canel@sinqia.com.br>
2019-05-20 15:37:08 -0300

    Ajuste threads clente

--- meuProjetoSD/src/com/suamSD/Cliente.java
+++ meuProjetoSD/src/com/suamSD/Cliente.java
@@ -11,96 +11,110 @@ import java.util.logging.Logger;
  * Cliente de uma aplicação Java RMI
  *
  */
-public class Cliente extends Thread
+public class Cliente extends Thread 
 {
-	private static final Runnable Cliente1 = new Cliente();
-	private static final Runnable Cliente2 = new Cliente();
+	private static final Runnable Cliente1 = new Cliente( );
+	private static final Runnable Cliente2 = new Cliente( );
 
-	public static void main(String[] args)
-    {
+	public static void main ( String[ ] args ) 
+	{
+        String ipServer = Util.defineIPservidor( );
+		
+		if ( ipServer == null )
+		{
+			System.out.println( "IP inválido" );
+			return;
+		}
 		
 		try 
 		{
-            // Obtendo referência do serviço de registro
-            Registry registro = LocateRegistry.getRegistry(Util.IPSERVIDOR, Util.PORTA);
+			// Obtendo referência do serviço de registro
+			Registry registro = LocateRegistry.getRegistry( ipServer, Util.PORTA );
 
-            // Procurando pelo objeto distribuído registrado previamente com o NOMEOBJDIST
-            AutomatoInterface stub = (AutomatoInterface) registro.lookup(Util.NOMEOBJDIST);
-            
-            stub.setContaThreads();
-            System.out.println("CONTADOR DE THREADS: " + stub.getContaThreads( ));
-            int thread = stub.getContaThreads( );
-            
-            switch (thread) {
-			case 1: 
-		            Thread  t = new Thread(Cliente1) 
-		            {
-		            	@Override
-			            public void run() 
-		            	{ 		
-					        try 
-					        {
-					        	stub.setAlfabeto 			 ( );
-					            //stub.setEstados  			 ( );
-					            stub.setRegra				 ( );
-					            //stub.setEstInicial			 ( );
-					            //stub.setConjuntoEstadosFinais( );
-					            stub.checaPalavra			 ( );
-							}
-					        catch ( RemoteException e ) 
-					        {
-								e.printStackTrace();
-							}
-			        	}
-		            }; //Cria a linha de execução
-		       	    t.start();                 //Ativa a thread
-		       	    t.setName( "Cliente: ==> "+ stub.getContaThreads( ));
+			// Procurando pelo objeto distribuído registrado previamente com o NOMEOBJDIST
+			AutomatoInterface stub = (AutomatoInterface) registro.lookup( Util.NOMEOBJDIST );
+
+			stub.setContaThreads( );
+			
+			System.out.println( "CONTADOR DE THREADS: " + stub.getContaThreads( ) );
+			
+			int thread = stub.getContaThreads( );
+
+			switch ( thread ) 
+			{
+			case 1:
+				Thread t = new Thread ( Cliente1 )
+				{
+					@Override
+					public void run( ) 
+					{
+						try 
+						{
+							stub.setAlfabeto( );
+							// stub.setEstados ( );
+							stub.setRegra( );
+							// stub.setEstInicial ( );
+							// stub.setConjuntoEstadosFinais( );
+							stub.checaPalavra( );
+						}
+						catch (RemoteException e) 
+						{
+							e.printStackTrace( );
+						}
+						// código para executar em paralelo
+						System.out.println( "ID: "         + Thread.currentThread( ).getId( )       );
+						System.out.println( "Nome: "       + Thread.currentThread( ).getName( )     );
+						System.out.println( "Prioridade: " + Thread.currentThread( ).getPriority( ) );
+						System.out.println( "Estado: "     + Thread.currentThread( ).getState( )    );
+					}
+				}; // Cria a linha de execução
+				t.start  ( ); // Ativa a thread
+				t.setName( "Cliente: ==> " + stub.getContaThreads( ) );
 				break;
-				
+
 			case 2:
-		        Thread  t2= new Thread(Cliente2) 
-		        {
-		        	@Override
-		            public void run() 
-		        	{ 		
-				        try 
-				        {
-				        	//stub.setAlfabeto 			 ( );
-							stub.setEstados  			 ( );
-							 //stub.setRegra				 ( );
-					        stub.setEstInicial			 ( );
-					        stub.setConjuntoEstadosFinais( );
-					        //stub.checaPalavra			 ( );
+				Thread t2 = new Thread( Cliente2 ) 
+				{
+					@Override
+					public void run( ) 
+					{
+						try 
+						{
+							// stub.setAlfabeto ( );
+							stub.setEstados( );
+							// stub.setRegra ( );
+							stub.setEstInicial( );
+							stub.setConjuntoEstadosFinais( );
+							// stub.checaPalavra ( );
+						} 
+						catch (RemoteException e) 
+						{
+							e.printStackTrace( );
 						}
-				        catch ( RemoteException e ) 
-				        {
-							e.printStackTrace();
-						}
-		        	}
-		        };
-		        //Cria a linha de execução
-		   	    t2.start();                 //Ativa a thread
-		   	    t2.setName( "Cliente: ==> "+ stub.getContaThreads( ));
+						
+						// código para executar em paralelo
+						System.out.println( "ID: "         + Thread.currentThread( ).getId( )       );
+						System.out.println( "Nome: "       + Thread.currentThread( ).getName( )     );
+						System.out.println( "Prioridade: " + Thread.currentThread( ).getPriority( ) );
+						System.out.println( "Estado: "     + Thread.currentThread( ).getState( )    );
+					}
+				};
+				// Cria a linha de execução
+				t2.start(); // Ativa a thread
+				t2.setName("Cliente: ==> " + stub.getContaThreads( ) );
 
 			default:
 				break;
 			}
-            
-        } catch (RemoteException | NotBoundException ex) {
-            Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
-        }
-		
-		
-  	    System.out.println("Fim da execução do cliente!");
-        
-        //código para executar em paralelo
-        System.out.println("ID: "         + Thread.currentThread( ).getId( )       );
-        System.out.println("Nome: "       + Thread.currentThread( ).getName( )     );
-        System.out.println("Prioridade: " + Thread.currentThread( ).getPriority( ) );
-        System.out.println("Estado: "     + Thread.currentThread( ).getState( )    );
-    
-	
-	
-}
-	
+
+		} 
+		catch (RemoteException | NotBoundException ex) 
+		{
+			Logger.getLogger( Cliente.class.getName( ) ).log( Level.SEVERE, null, ex );
+		}
+
+		System.out.println( "Fim da execução do cliente!" );
+
+	}
+
 }
